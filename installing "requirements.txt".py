9891a55d8addc91998de7d9ce2ae8618d7f233d5
Mailapalli Avinash <91531224+Royal-ash@users.noreply.github.com>
2023-08-02 11:44:01 +0530

    Update and rename installing requirements.txt to installing "requirements.txt".py

--- installing "requirements.txt".py
+++ installing "requirements.txt".py
@@ -3,3 +3,5 @@ pip install -r requirements.txt
 #if not installing then first freeze all requirements then write code for installing
 pip freeze > requirements.txt
 pip install -r requirements.txt
+#if showing warning for permission then
+pip install --user -r requirements.txt 
